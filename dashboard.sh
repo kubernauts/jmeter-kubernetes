b9c480cfc9a9351e39ce37772581007b787c786e
Christopher Adigun <ca@cloudssky.com>
2018-02-10 19:39:00 +0100

    Addded scripts and manifest files

---
+++ dashboard.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+working_dir=`pwd`
+
+#Get namesapce variable
+tenant=`awk '{print $NF}' $working_dir/tenant_export`
+
+## Create jmeter database automatically in Influxdb
+
+echo "Creating Influxdb jmeter Database"
+
+##Wait until Influxdb Deployment is up and running
+##influxdb_status=`kubectl get po -n $tenant | grep influxdb-jmeter | awk '{print $2}' | grep Running
+
+influxdb_pod=`kubectl get po -n $tenant | grep influxdb-jmeter | awk '{print $1}'`
+kubectl exec -ti -n $tenant $influxdb_pod -- influx -execute 'CREATE DATABASE jmeter'
+
+## Create the influxdb datasource in Grafana
+
+echo "Creating the Influxdb data source"
+grafana_pod=`kubectl get po -n $tenant | grep jmeter-grafana | awk '{print $1}'`
+
+## Make load test script in Jmeter master pod executable
+
+#Get Master pod details
+
+master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
+
+kubectl exec -ti -n $tenant $master_pod -- chmod 755 /root/load_test
+
+##kubectl cp $working_dir/influxdb-jmeter-datasource.json -n $tenant $grafana_pod:/influxdb-jmeter-datasource.json
+
+kubectl exec -ti -n $tenant $grafana_pod -- curl 'http://admin:admin@127.0.0.1:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"jmeterdb","type":"influxdb","url":"http://jmeter-influxdb:8086","access":"proxy","isDefault":true,"database":"jmeter","user":"admin","password":"admin"}'
---
+++ start_test.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+#Script created to launch Jmeter tests directly from the current terminal without accessing the jmeter master pod.
+#It requires that you supply the path to the jmx file
+#After execution, test script jmx file may be deleted from the pod itself but not locally.
+
+working_dir=`pwd`
+
+#Get namesapce variable
+tenant=`awk '{print $NF}' $working_dir/tenant_export`
+
+read -p 'Enter path tp the jmx file ' jmx
+
+if [ ! -f "$jmx" ];
+then
+    echo "Test script file was not found in PATH"
+    echo "Kindly check and input the correct file path"
+    exit
+fi
+
+#Get Master pod details
+
+master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
+
+kubectl cp $jmx -n $tenant $master_pod:/$jmx
+
+## Echo Starting Jmeter load test
+
+kubectl exec -ti -n $tenant $master_pod -- /root/load_test $jmx
