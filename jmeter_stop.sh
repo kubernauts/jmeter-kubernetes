481f819a3629cf835aa560e88378e79f53ab1fa3
brentchang <acegi123@gmail.com>
2020-01-16 15:01:57 +0800

    Fixed hard-coded version number in script

--- jmeter_stop.sh
+++ jmeter_stop.sh
@@ -9,4 +9,4 @@ tenant=`awk '{print $NF}' $working_dir/tenant_export`
 
 master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
 
-kubectl -n $tenant exec -ti $master_pod bash /jmeter/apache-jmeter-5.0/bin/stoptest.sh
+kubectl -n $tenant exec -ti $master_pod -- /bin/bash -c '/jmeter/apache-jmeter-*/bin/stoptest.sh'
