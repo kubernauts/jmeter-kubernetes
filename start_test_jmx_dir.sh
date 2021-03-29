9b0110a24ae8665afe6d3b469a92d38c10e44d7f
Hao Shen <hao.shen@cognizant.com>
2021-03-30 05:44:00 +0800

    added start_test_jmx_dir.sh

--- start_test_jmx_dir.sh
+++ start_test_jmx_dir.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# https://betterdev.blog/minimal-safe-bash-script-template/
 
 set -Eeuo pipefail
 trap cleanup SIGINT SIGTERM ERR EXIT
@@ -7,20 +8,19 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 usage() {
   cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] 
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] jmx_dir
 
 To launch Jmeter tests directly from the current terminal without accessing the jmeter master pod.
-It requires that you supply the directory containing to the jmx file, and all supporting files that associated with the test such as propeties and csv files.
-The directory must contain ONE, and ONLY ONE, jmx file.
-After execution, test script jmx file may be deleted from the pod itself but not locally.
+It requires that you supply the test plan directory (jmx_dir), which contains ONLY ONE jmx file at the surface level.
+The directory may contain additional supporting files, such as propeties and csv files.
+The entire directory will be copied into the jmeter_master pod, and the jmx file at the surface will be executed.
+After execution, test script jmx file may be deleted from the master pod but not locally.
 
 Available options:
 
 -h, --help      Print this help and exit
 -v, --verbose   Print script debug info
 EOF
-# -f, --flag      Some flag description
-# -p, --param     Some param description
   exit
 }
 
@@ -49,20 +49,12 @@ die() {
 }
 
 parse_params() {
-  # default values of variables set from params
-  # flag=0
-  # param=''
 
   while :; do
     case "${1-}" in
     -h | --help) usage ;;
     -v | --verbose) set -x ;;
     --no-color) NO_COLOR=1 ;;
-    # -f | --flag) flag=1 ;; # example flag
-    # -p | --param) # example named parameter
-    #   param="${2-}"
-    #   shift
-    #   ;;
     -?*) die "Unknown option: $1" ;;
     *) break ;;
     esac
@@ -72,8 +64,7 @@ parse_params() {
   args=("$@")
 
   # check required params and arguments
-  # [[ -z "${param-}" ]] && die "Missing required parameter: param"
-  [[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
+  [[ ${#args[@]} -eq 0 ]] && die "Missing jmx_dir, directory containing the jmx test plan."
 
   return 0
 }
@@ -81,44 +72,38 @@ parse_params() {
 parse_params "$@"
 setup_colors
 
-# script logic here
-
-# msg "${RED}Read parameters:${NOFORMAT}"
-# msg "- flag: ${flag}"
-# msg "- param: ${param}"
-# msg "- arguments: ${args[*]-}"
-
-
-
-working_dir="`pwd`"
-
-#Get namesapce variable
+# Get namesapce variable stored in tenant_export.
 tenant=`awk '{print $NF}' "$script_dir/tenant_export"`
+jmx_dir="$1"
 
-jmx="$1"
-echo $tenant
-echo $jmx
+# Assert there is only one jmx file at the surface level. 
+if [ ! -d "$jmx_dir" ]
+then
+  msg "Directory $jmx_dir does not exist!"
+  msg "Kindly check and input the correct directory."
+  exit
+else
+  jmx_num=`find $jmx_dir -maxdepth 1 -name "*.jmx" | wc -l | xargs`
+  if [ $((jmx_num)) -eq 0 ]
+  then
+    msg "Directory $jmx_dir does not contain any jmx file at the surface level."
+    exit
+  elif [ $((jmx_num)) -gt 1 ]
+  then
+    msg "Directory $jmx_dir has multiple jmx files at the surface level. Only one is allowed."
+    exit
+  fi
+fi
 
+# Get FQN of the jmx file
+jmx_name=`find $jmx_dir -maxdepth 1 -name "*.jmx"`
 
-# [ -n "$jmx" ] || read -p 'Enter path to the jmx file ' jmx
+# Get Master pod details
+master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
 
-# if [ ! -f "$jmx" ];
-# then
-#     echo "Test script file was not found in PATH"
-#     echo "Kindly check and input the correct file path"
-#     exit
-# fi
+msg "Coping test files into jmeter-master pod $master_pod:/tmp/$jmx_dir ..."
+kubectl exec -ti -n $tenant $master_pod  -- rm -rf /tmp/$jmx_dir
+kubectl cp $jmx_dir -n $tenant $master_pod:/tmp/$jmx_dir
 
-# test_name="$(basename "$jmx")"
-
-# echo $test_name
-
-# #Get Master pod details
-
-# master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
-
-# kubectl cp "$jmx" -n $tenant "$master_pod:/$test_name"
-
-# ## Echo Starting Jmeter load test
-
-# kubectl exec -ti -n $tenant $master_pod -- /bin/bash /load_test "$test_name"
+msg "Starting JMeter load test..."
+kubectl exec -ti -n $tenant $master_pod -- /bin/bash /load_test /tmp/$jmx_name
