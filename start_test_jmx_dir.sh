b44234b3d416aeb81bf8ebd41ee7d637564cb02e
Hao Shen <hao.shen@cognizant.com>
2021-03-31 10:26:42 +0800

    adding the ability to use properties

--- start_test_jmx_dir.sh
+++ start_test_jmx_dir.sh
@@ -8,11 +8,15 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 usage() {
   cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] test_plan_dir jmx_file properties_file test_report_name
+
+Usage: 
+
+$(basename "${BASH_SOURCE[0]}") [-h] test_plan_dir jmx_file properties_file test_report_name
+
 test_plan_dir: The test plan directory.
 jmx_file: The jmeter test file name.
-properties_file: The properties file name to be used with the jmx.
-test_report_name: Name for the generated JMeter test report and output log.
+properties_file: The properties file name to be used with the jmx. Must be at the surface level in the test plan directory.
+test_report_name: Name for the generated JMeter test report and output log. Must be at the surface level in the test plan directory.
 
 To launch Jmeter tests directly from the current terminal without accessing the jmeter-master pod.
 It requires that you supply the test plan directory (test_plan_dir), which contains jmx_file and properties_file at the surface level.
@@ -24,6 +28,7 @@ Available options:
 
 -h, --help      Print this help and exit
 -v, --verbose   Print script debug info
+
 EOF
   exit
 }
@@ -78,45 +83,43 @@ setup_colors
 
 # Get namesapce variable stored in tenant_export.
 tenant=`awk '{print $NF}' "$script_dir/tenant_export"`
-test_plan_dir jmx_file properties_file test_report_name
+# test_plan_dir jmx_file properties_file test_report_name
 test_plan_dir="$1"
 jmx_file="$2"
 properties_file="$3"
 test_report_name="$4"
 
 # Assert jmx_dir exsists, and the jmx_file and properties_file are located at its surface level. 
-if [ ! -d "$jmx_dir" ]
+if [ ! -d "$test_plan_dir" ]
 then
-  msg "Directory $jmx_dir does not exist!"
-  msg "Kindly check and input the correct directory."
-  exit
+  die "Directory $test_plan_dir does not exist! Use -h for help."
 else
-  jmx_num=`find $jmx_dir -maxdepth 1 -name "*.jmx" | wc -l | xargs`
-  if [ $((jmx_num)) -eq 0 ]
+  #### TODO
+  # TODO: parsing jmx and properties name
+  ##############################
+  if [ ! -f "$test_plan_dir/$jmx_file.jmx" ]
   then
-    msg "Directory $jmx_dir does not contain any jmx file at the surface level."
-    exit
-  elif [ $((jmx_num)) -gt 1 ]
+    die "$jmx_file.jmx does not exist in $test_plan_dir at the surface level.  Use -h for help"
+  elif [ ! -f "$test_plan_dir/$properties_file.properties" ]
   then
-    msg "Directory $jmx_dir has multiple jmx files at the surface level. Only one is allowed."
-    exit
+    die "$properties_file.properties does not exist in $test_plan_dir at the surface level.  Use -h for help"
   fi
 fi
 
-exit
+
 
 # Get FQN of the jmx file
-jmx_file=`find $jmx_dir -maxdepth 1 -name "*.jmx"`
+# jmx_file=`find $test_plan_dir -maxdepth 1 -name "*.jmx"`
 
 # Get Master pod details
 master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
 
-msg "Pushing test files into jmeter-master pod $master_pod:/tmp/$jmx_dir ..."
-kubectl exec -ti -n $tenant $master_pod  -- rm -rf /tmp/$jmx_dir
-kubectl cp $jmx_dir -n $tenant $master_pod:/tmp/$jmx_dir
+msg "Pushing test files into jmeter-master pod $master_pod:/tmp/$test_plan_dir ..."
+kubectl exec -ti -n $tenant $master_pod  -- rm -rf /tmp/$test_plan_dir
+kubectl cp $test_plan_dir -n $tenant $master_pod:/tmp/$test_plan_dir
 
-msg "Starting the JMeter test..."
-jtl_exists=`kubectl exec -ti -n $tenant $master_pod -- find /tmp -maxdepth 1 -name ${test_report_name}.jtl | wc -l`
+# Checking if the test_report_name already exists in the jmeter-master pod, if so add date.
+jtl_exists=`kubectl exec -ti -n $tenant $master_pod -- find /tmp -maxdepth 1 -name ${test_report_name}.jtl | wc -l | xargs`
 if [ $((jtl_exists)) -eq 1 ]
 then
   now=`date +"%H%M%S_%Y%b%d"`
@@ -124,7 +127,9 @@ then
   msg "${test_report_name} already esists in the jmeter master pod. Renaming it with current date as ${new_test_report_name}"
   test_report_name=$new_test_report_name
 fi
-kubectl exec -ti -n $tenant $master_pod -- /bin/bash /load_test /tmp/$jmx_file /tmp/$test_report_name.jtl
+
+msg "Starting the JMeter test..."
+kubectl exec -ti -n $tenant $master_pod -- /bin/bash /load_test /tmp/$test_plan_dir/$jmx_file.jmx /tmp/$test_plan_dir/$properties_file.properties /tmp/$test_report_name.jtl
 
 msg "Generating the JMeter HTML report..."
 kubectl exec -ti -n $tenant $master_pod -- /bin/bash /generate_report /tmp/$test_report_name.jtl /tmp/$test_report_name
