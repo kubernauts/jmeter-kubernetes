37035f9206ead89d1827cc77d745d891b9a5bfcd
Hao Shen <hao.shen@cognizant.com>
2021-04-01 16:56:46 +0800

    add a blank line

--- setup_grafana_dashboard.sh
+++ setup_grafana_dashboard.sh
@@ -32,4 +32,6 @@ kubectl exec -ti -n $tenant $master_pod -- chmod 755 /jmeter/load_test
 
 kubectl exec -ti -n $tenant $grafana_pod -- curl 'http://admin:admin@127.0.0.1:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"jmeterdb","type":"influxdb","url":"http://jmeter-influxdb:8086","access":"proxy","isDefault":true,"database":"jmeter","user":"admin","password":"admin"}'
 
+echo
+
 # kubectl cp $working_dir/influxdb-jmeter-datasource.json -n $tenant $grafana_pod:/influxdb-jmeter-datasource.json
